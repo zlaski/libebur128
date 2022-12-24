0f1544ec828ba48506fdb79a77a0a174ba78a953
Ziemowit Łąski <15880281+zlaski@users.noreply.github.com>
2022-12-23 20:14:10 -0800

    Update build-cygwin.h and .gitignore

--- build-cygwin.sh
+++ build-cygwin.sh
@@ -1,17 +1,19 @@
-#!/bin/bash -x
+#!/bin/bash
 
-cd $(dirname $0)
+SCRIPT=`basename $0 .sh`
+SCRIPT_DIR=$(realpath $(dirname $0))
+pushd $SCRIPT_DIR
 
-export BUILD_SHARED_LIBS=OFF
-export CMAKE_INSTALL_PREFIX=$PWD/.install
-export CMAKE_C_FLAGS=-fpermissive
-export CMAKE_CXX_FLAGS=-fpermissive
-
-rm -rf .build .install
-mkdir -p .build
+echo "################### BUILDING $0 ####################"
+    
+rm -rf .build || exit 1
+mkdir -p .build || exit 1
 cd .build
 
-cmake -G 'Unix Makefiles' -DBUILD_SHARED_LIBS=$BUILD_SHARED_LIBS -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX \
-                          -DCMAKE_C_FLAGS=$CMAKE_C_FLAGS -DCMAKE_CXX_FLAGS=$CMAKE_CXX_FLAGS ..
-make
-make install
+cmake --install-prefix=$CMAKE_INSTALL_PREFIX -DBUILD_SHARED_LIBS=$BUILD_SHARED_LIBS \
+    -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_C_FLAGS=$CMAKE_C_FLAGS -DCMAKE_CXX_FLAGS=$CMAKE_CXX_FLAGS \
+    -DCMAKE_STATIC_LINKER_FLAGS=-v --debug-trycompile -G 'Unix Makefiles' .. || exit 1
+make SHELL='/bin/bash -x' || exit 1
+make install SHELL='/bin/bash -x' || exit 1
+
+popd
