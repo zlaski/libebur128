73456e696eb4f982762311e3cf7977e4484ffc1c
Ziemowit Łąski <15880281+zlaski@users.noreply.github.com>
2023-02-12 22:49:48 -0800

    akibld-friendly msvc project

--- libebur128.post.bat
+++ libebur128.post.bat
@@ -1,9 +1,11 @@
 @echo off
 setlocal EnableExtensions EnableDelayedExpansion
 
-set ProjectDir=%~dp0
-
-set OutDir=%~1
+set "ProjectDir=%~dp0"
+set "SolutionPath=%~1"
+set "IntDir=%~2"
+set "OutDir=%~3"
 
 attrib -R %OutDir%..\include\ebur128.h >nul
 copy /y %ProjectDir%ebur128\ebur128.h %OutDir%..\include\ebur128.h
+attrib +R %OutDir%..\include\ebur128.h >nul
